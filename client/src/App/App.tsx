4f0d5fffb950cfde8f0d0dc2a92e3299f0be1abb
Bashkatov-spb <bashkatov.spb@mail.ru>
2023-08-29 09:37:11 +0300

    ex init

---
+++ client/src/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h1>Tut vashe prilojenie</h1>
+    </div>
+  );
+}
+
+export default App;
